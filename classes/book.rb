848a4abc908bac9cc6bff86703ae6a339433fffc
Juanpa8830 <jp3_4@hotmail.com>
2023-03-28 11:11:30 -0500

    Add class Book

---
+++ classes/book.rb
@@ -0,0 +1,18 @@
+require 'securerandom'
+require_relative 'item'
+
+class Book < Item
+  attr_accessor :publisher, :cover_state
+
+  def initialize(*args, publisher, cover_state)
+    super(*args)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def can_be_archived?
+    return true if super || @cover_state == 'bad'
+
+    false
+  end
+end
