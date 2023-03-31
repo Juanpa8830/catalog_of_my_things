af0a6a3307c52394373cca14fcc2877133a7783a
Juanpa8830 <jp3_4@hotmail.com>
2023-03-31 13:06:25 -0500

    Add Label class with list/preserve info methods

--- classes/label.rb
+++ classes/label.rb
@@ -2,15 +2,25 @@ require 'securerandom'
 
 
 class Label
-  attr_accessor :title, :color
+  attr_accessor :title, :color, :items, :id
 
-  def initialize(title, _color)
-    @id = SecureRandom.uuid
+  def initialize(title, color, id)
+    @id = id
     @title = title
+    @color = color
     @items = []
   end
 
   def add_item(item)
     @items.push(item) unless @items.include?(item)
+    item.label = self
+  end
+
+  def self.list_all_labels
+    ObjectSpace.each_object(self).to_a
+  end
+
+  def self.by_id(id)
+    list_all_labels.find { |label| label.id == id }
   end
 end
