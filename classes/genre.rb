10ed36c8891a4b576d0a6aa5565764cb9a267fd7
Juanpa8830 <jp3_4@hotmail.com>
2023-03-31 13:07:46 -0500

    Edit Genre class to list/preserve info

--- classes/genre.rb
+++ classes/genre.rb
@@ -7,48 +7,18 @@ class Genre
     @id = id
     @name = name
     @items = []
-    save_to_file
   end
 
   def add_item(item)
-    @items << item
+    @items << item unless @items.include?(item)
     item.genre = self
-    save_to_file
   end
 
   def self.list_all_genres
-    genres = []
-    ObjectSpace.each_object(self) do |genre|
-      genres << genre.name
-    end
-    genres
+    ObjectSpace.each_object(self).to_a
   end
 
-  def self.list_all_music_albums
-    albums = []
-    ObjectSpace.each_object(self) do |genre|
-      genre.items.each do |item|
-        albums << item if item.is_a?(MusicAlbum)
-      end
-    end
-    albums
-  end
-
-  def add_music_album(id, author, label, publish_date, archived, on_spotify)
-    album = MusicAlbum.new(id, @name, author, label, publish_date, archived, on_spotify)
-    add_item(album)
-  end
-
-  def save_to_file
-    file_path = "data/genre_data.json"
-    data = JSON.parse(File.read(file_path)) rescue []
-    genre_data = { id: @id, name: @name, items: @items.map(&:to_h) }
-    existing_genre_data = data.find { |d| d["id"] == @id }
-    if existing_genre_data
-      existing_genre_data.update(genre_data)
-    else
-      data << genre_data
-    end
-    File.write(file_path, JSON.pretty_generate(data))
+  def self.by_id(id)
+    list_all_genres.find { |genre| genre.id == id }
   end
 end
